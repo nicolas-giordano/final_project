6189df0a8518c0ffcd1796029232da8c731ebb33
Nicolas Giordano <47097496+nicolas-giordano@users.noreply.github.com>
2021-04-22 15:51:11 -0500

    bare-bones design ready to start testing API implementation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
-import Search from './Search.js';
 import Trending from './Trending.js';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <>
-      <Header />
-      <Search />
-      <section id='anime__listings'>
-        <Trending />
-      </section>
-      </>
-    )
-  }
+	render() {
+		return (
+			<>
+				<Header />
+				<section id="anime__listings">
+					<Trending />
+				</section>
+			</>
+		);
+	}
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,29 +1,39 @@
-import React, { Component} from 'react';
-import './Header.css'
+import React, { Component } from 'react';
+import { BsSearch } from 'react-icons/bs';
+import './Header.css';
 
 class Header extends Component {
-    render() {
-        return (
-            <section className='header'>
-                <span className='header__item'>
-                    Home
-                </span>
-                <div className='categories__menu'>
-                    <span id='category' className='header__item'>
-                        Categories
-                    </span>
-                    <div className='dropdown'>
-                        <a>Test1</a>
-                        <a>Test2</a>
-                        <a>Test3</a>
-                    </div>
-                </div>
-                <span className='header__item'>
-                    Top 100
-                </span>
-            </section>
-        )
-    }
+	render() {
+		return (
+			<section className="header">
+				<div className="header__left">
+					<span className="header__item">Home</span>
+					<div className="categories__menu">
+						<span id="category" className="header__item">
+							Categories
+						</span>
+						<div className="dropdown">
+							<a>Test1</a>
+							<a>Test2</a>
+							<a>Test3</a>
+						</div>
+					</div>
+					<span className="header__item">Top 100</span>
+				</div>
+				<div className="header__right">
+					<form>
+						<BsSearch className="search__icon" />
+						<input
+							type="text"
+							id="header__search"
+							placeholder="Search for a title..."
+							autoComplete="none"
+						/>
+					</form>
+				</div>
+			</section>
+		);
+	}
 }
 
 export default Header;
