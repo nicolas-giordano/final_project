9682e2d294ae3c3603d0dd6dd862b2f954f7fae6
nicolas-giordano <nickgior@gmail.com>
2021-04-21 22:17:24 -0700

    worked on website design a little bit more

--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,23 @@ import './Header.css'
 class Header extends Component {
     render() {
         return (
-            <section id='header'>
-                Home    Categories  Top 100
+            <section className='header'>
+                <span className='header__item'>
+                    Home
+                </span>
+                <div className='categories__menu'>
+                    <span id='category' className='header__item'>
+                        Categories
+                    </span>
+                    <div className='dropdown'>
+                        <a>Test1</a>
+                        <a>Test2</a>
+                        <a>Test3</a>
+                    </div>
+                </div>
+                <span className='header__item'>
+                    Top 100
+                </span>
             </section>
         )
     }
--- src/Search.js
+++ src/Search.js
@@ -1,15 +1,19 @@
 import React, { Component} from 'react';
 import './Search.css'
-
+import { BsSearch } from 'react-icons/bs';
 
 class Search extends Component {
 
     render() {
         return (
             <>
-            <form>
-                <input type='text' placeholder='Search a title...'/>
-            </form>
+            <section className='search__input'>
+                <h2>Looking for something specific?</h2>
+                <form>
+                    <BsSearch size='2em' className='search__icon'/> 
+                    <input type='text' id='anime__input' placeholder='Search a title...' autoComplete='none'/>
+                </form> 
+            </section>
             </>
         )
     }
